07fc26f3e0edd3e5a8a3a195a8703d5afe1b2f4a
Will Gantt <wgantt.iv@gmail.com>
2020-07-17 14:35:16 -0400

    add no grad environment to eval

--- nli_mixed_models/eval/nli_eval.py
+++ nli_mixed_models/eval/nli_eval.py
@@ -42,88 +42,89 @@ class NaturalLanguageInferenceEval:
 
         self.nli.eval()
         
-        n_batches = np.ceil(test_data.shape[0]/batch_size)
-        test_data.loc[:,'batch_idx'] = np.repeat(np.arange(n_batches), batch_size)[:test_data.shape[0]]
+        with torch.no_grad():
+          n_batches = np.ceil(test_data.shape[0]/batch_size)
+          test_data.loc[:,'batch_idx'] = np.repeat(np.arange(n_batches), batch_size)[:test_data.shape[0]]
 
-        loss_trace = []
-        fixed_loss_trace = []
-        random_loss_trace = []
-        metric_trace = []
-        best_trace = []
-        spearman_trace = []
+          loss_trace = []
+          fixed_loss_trace = []
+          random_loss_trace = []
+          metric_trace = []
+          best_trace = []
+          spearman_trace = []
 
-        # Calculate metrics for each batch in test set
-        for batch, items in test_data.groupby('batch_idx'):
-          LOG.info('evaluating batch [%s/%s]' % (int(batch), int(n_batches)))
+          # Calculate metrics for each batch in test set
+          for batch, items in test_data.groupby('batch_idx'):
+            LOG.info('evaluating batch [%s/%s]' % (int(batch), int(n_batches)))
 
-          if self.subtask == 'a':
-            participant = torch.LongTensor(items.participant.values)
-          else:
-            participant = None
-    
-          # Get target values of appropriate type
-          if isinstance(self.nli, CategoricalRandomIntercepts) or isinstance(self.nli, CategoricalRandomSlopes):
-            target = torch.LongTensor(items.target.values).to(self.device)
-            modal_response = torch.LongTensor(items.modal_response.values).to(self.device)
-          else:
-            target = torch.FloatTensor(items.target.values).to(self.device)
-            modal_response = torch.FloatTensor(items.modal_response.values).to(self.device)
+            if self.subtask == 'a':
+              participant = torch.LongTensor(items.participant.values)
+            else:
+              participant = None
+      
+            # Get target values of appropriate type
+            if isinstance(self.nli, CategoricalRandomIntercepts) or isinstance(self.nli, CategoricalRandomSlopes):
+              target = torch.LongTensor(items.target.values).to(self.device)
+              modal_response = torch.LongTensor(items.modal_response.values).to(self.device)
+            else:
+              target = torch.FloatTensor(items.target.values).to(self.device)
+              modal_response = torch.FloatTensor(items.modal_response.values).to(self.device)
 
-          # Embed items    
-          embedding = self.nli.embed(items)
+            # Embed items    
+            embedding = self.nli.embed(items)
 
-          # Calculate model prediction and compute fixed & random loss
-          if isinstance(self.nli, UnitRandomIntercepts) or \
-             isinstance(self.nli, UnitRandomSlopes):
-            prediction, random_loss = self.nli(embedding, participant)
-            alpha, beta = prediction
-            prediction = self.TARGET_TYPE(beta_mode(alpha, beta)).to(self.device)
-            fixed_loss = self.lossfunc(alpha, beta, target)       
-          else:
-            prediction, random_loss = self.nli(embedding, participant)
-            fixed_loss = self.lossfunc(prediction, target)
+            # Calculate model prediction and compute fixed & random loss
+            if isinstance(self.nli, UnitRandomIntercepts) or \
+               isinstance(self.nli, UnitRandomSlopes):
+              prediction, random_loss = self.nli(embedding, participant)
+              alpha, beta = prediction
+              prediction = self.TARGET_TYPE(beta_mode(alpha, beta)).to(self.device)
+              fixed_loss = self.lossfunc(alpha, beta, target)       
+            else:
+              prediction, random_loss = self.nli(embedding, participant)
+              fixed_loss = self.lossfunc(prediction, target)
 
-          random_loss = random_loss if isinstance(random_loss, float) else random_loss.item()
+            random_loss = random_loss if isinstance(random_loss, float) else random_loss.item()
 
-          # Add total loss to trace
-          loss = fixed_loss + random_loss
-          loss_trace.append(loss.item())
-          fixed_loss_trace.append(fixed_loss.item())
-          random_loss_trace.append(random_loss)
+            # Add total loss to trace
+            loss = fixed_loss + random_loss
+            loss_trace.append(loss.item())
+            fixed_loss_trace.append(fixed_loss.item())
+            random_loss_trace.append(random_loss)
 
-          # If categorical, calculate accuracy (and Spearman's coefficient)
-          if isinstance(self.nli, CategoricalRandomIntercepts) or \
-            isinstance(self.nli, CategoricalRandomSlopes):
-            acc = accuracy(prediction, target)
-            best = accuracy_best(items)
-            metric_trace.append(acc)
-            best_trace.append(acc/best)
+            # If categorical, calculate accuracy (and Spearman's coefficient)
+            if isinstance(self.nli, CategoricalRandomIntercepts) or \
+              isinstance(self.nli, CategoricalRandomSlopes):
+              acc = accuracy(prediction, target)
+              best = accuracy_best(items)
+              metric_trace.append(acc)
+              best_trace.append(acc/best)
 
-            # Calculate Spearman
-            spearman_df = pd.DataFrame()
-            spearman_df['true'] = pd.Series(target.cpu().detach().numpy())
-            spearman_df['predicted'] = pd.Series(prediction.argmax(1).cpu().detach().numpy())
+              # Calculate Spearman
+              spearman_df = pd.DataFrame()
+              spearman_df['true'] = pd.Series(target.cpu().detach().numpy())
+              spearman_df['predicted'] = pd.Series(prediction.argmax(1).cpu().detach().numpy())
 
-                    
-          # If unit, calculate absolute error
-          else:
-            error = absolute_error(prediction, target)
-            best = absolute_error(modal_response, target)
-            metric_trace.append(error)
-            best_trace.append(1 - (error-best)/best)
+                      
+            # If unit, calculate absolute error
+            else:
+              error = absolute_error(prediction, target)
+              best = absolute_error(modal_response, target)
+              metric_trace.append(error)
+              best_trace.append(1 - (error-best)/best)
 
-          spearman = spearman_df.corr().iloc[0,1]
-          spearman_trace.append(spearman)
+            spearman = spearman_df.corr().iloc[0,1]
+            spearman_trace.append(spearman)
 
-        # Calculate and return mean of metrics across all batches
-        loss_mean = np.round(np.mean(loss_trace), 4)
-        fixed_loss_mean = np.round(np.mean(fixed_loss_trace), 4)
-        random_loss_mean = np.round(np.mean(random_loss_trace), 4)
-        metric_mean = np.round(np.mean(metric_trace), 4)
-        best_mean = np.round(np.mean(best_trace), 4)
-        spearman_mean = np.round(np.mean(spearman_trace), 4)
+          # Calculate and return mean of metrics across all batches
+          loss_mean = np.round(np.mean(loss_trace), 4)
+          fixed_loss_mean = np.round(np.mean(fixed_loss_trace), 4)
+          random_loss_mean = np.round(np.mean(random_loss_trace), 4)
+          metric_mean = np.round(np.mean(metric_trace), 4)
+          best_mean = np.round(np.mean(best_trace), 4)
+          spearman_mean = np.round(np.mean(spearman_trace), 4)
 
-        return loss_mean, fixed_loss_mean, random_loss_mean, metric_mean, best_mean, spearman_mean
+          return loss_mean, fixed_loss_mean, random_loss_mean, metric_mean, best_mean, spearman_mean
 
 
 # Unit eval
