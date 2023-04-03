Updated model to allow for per-item fixed variance terms when 'use_item_variance' hyperparam is given

nli_mixed_models/eval/nli_eval.py
@@ -79,6 +79,8 @@ class NaturalLanguageInferenceEval:
                 else:
                     participant = None
 
+                item = torch.LongTensor(items.item.values).to(self.device)
+
                 # Get target values of appropriate type
                 if isinstance(self.nli, CategoricalRandomIntercepts) or isinstance(
                     self.nli, CategoricalRandomSlopes
@@ -102,12 +104,12 @@ class NaturalLanguageInferenceEval:
                 if isinstance(self.nli, UnitRandomIntercepts) or isinstance(
                     self.nli, UnitRandomSlopes
                 ):
-                    prediction, random_loss = self.nli(embedding, participant)
+                    prediction, random_loss = self.nli(embedding, participant, item)
                     alpha, beta = prediction
                     prediction = alpha / (alpha + beta)
                     fixed_loss = self.lossfunc(alpha, beta, target)
                 else:
-                    prediction, random_loss = self.nli(embedding, participant)
+                    prediction, random_loss = self.nli(embedding, participant, item)
                     fixed_loss = self.lossfunc(prediction, target)
                 all_predictions = torch.cat((all_predictions, prediction))
 

nli_mixed_models/modules/nli_base.py
@@ -18,7 +18,9 @@ class NaturalLanguageInference(Module):
         hidden_dim: int,
         output_dim: int,
         n_participants: int,
+        n_items: int,
         setting: str,
+        use_item_variance: bool,
         use_sampling: bool,
         n_samples: int,
         device=torch.device("cpu"),
@@ -32,13 +34,15 @@ class NaturalLanguageInference(Module):
         self.hidden_dim = hidden_dim
         self.output_dim = output_dim
         self.n_participants = n_participants
+        self.n_items = n_items
         self.setting = setting
+        self.use_item_variance = use_item_variance
         self.use_sampling = use_sampling
         self.n_samples = n_samples
         self.device = device
 
     def forward(
-        self, embeddings, participant=None
+        self, embeddings, participant=None, item=None
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         """Abstract method to be overridden by subclasses."""
         prediction, random_loss = None, None

nli_mixed_models/modules/nli_random_intercepts.py
@@ -20,7 +20,9 @@ class RandomInterceptsModel(NaturalLanguageInference):
         hidden_dim: int,
         output_dim: int,
         n_participants: int,
+        n_items: int,
         setting: str,
+        use_item_variance: bool,
         use_sampling: bool,
         n_samples: int,
         device=torch.device("cpu"),
@@ -31,7 +33,9 @@ class RandomInterceptsModel(NaturalLanguageInference):
             hidden_dim,
             output_dim,
             n_participants,
+            n_items,
             setting,
+            use_item_variance,
             use_sampling,
             n_samples,
             device,
@@ -63,7 +67,7 @@ class RandomInterceptsModel(NaturalLanguageInference):
         return Sequential(*seq)
 
     def forward(
-        self, embeddings, participant=None
+        self, embeddings, participant=None, item=None
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         """Do a forward pass on the model. Returns a tuple (prediction, random_loss), where
            random_loss is a loss associated with the prior over the random components.
@@ -76,7 +80,7 @@ class RandomInterceptsModel(NaturalLanguageInference):
         if self.setting == "standard":
             random = None
             random_loss = 0.0
-            prediction = self._link_function(fixed, random, participant)
+            prediction = self._link_function(fixed, random, participant, item)
             if isinstance(self, UnitRandomIntercepts):
                 alpha, beta = prediction
 
@@ -90,7 +94,7 @@ class RandomInterceptsModel(NaturalLanguageInference):
             if participant is None and self.use_sampling:
                 predictions = []
                 for i in range(self.n_samples):
-                    predictions.append(self._link_function(fixed, random, participant))
+                    predictions.append(self._link_function(fixed, random, participant, item))
                 # If unit case, predictions will be (alpha, beta) tuples
                 if all(isinstance(x, tuple) for x in predictions):
                     alpha = torch.stack([x[0] for x in predictions]).mean(0)
@@ -99,7 +103,7 @@ class RandomInterceptsModel(NaturalLanguageInference):
                 else:
                     prediction = torch.stack(predictions).mean(0)
             else:
-                prediction = self._link_function(fixed, random, participant)
+                prediction = self._link_function(fixed, random, participant, item)
 
         return prediction, random_loss
 
@@ -112,7 +116,9 @@ class CategoricalRandomIntercepts(RandomInterceptsModel):
         hidden_dim: int,
         output_dim: int,
         n_participants: int,
+        n_items: int,
         setting: str,
+        use_item_variance: bool,
         use_sampling: bool,
         n_samples: int,
         device=torch.device("cpu"),
@@ -123,7 +129,9 @@ class CategoricalRandomIntercepts(RandomInterceptsModel):
             hidden_dim,
             output_dim,
             n_participants,
+            n_items,
             setting,
+            use_item_variance,
             use_sampling,
             n_samples,
             device=device,
@@ -141,7 +149,7 @@ class CategoricalRandomIntercepts(RandomInterceptsModel):
         """Returns the mean-subtracted random effects of the model."""
         return self.random_effects - self.random_effects.mean(0)[None, :]
 
-    def _link_function(self, fixed, random, participant):
+    def _link_function(self, fixed, random, participant, item):
         """Computes the link function for a given model configuration."""
         # Standard setting: no annotators, so no random effects.
         if self.setting == "standard" or random is None:
@@ -186,7 +194,9 @@ class UnitRandomIntercepts(RandomInterceptsModel):
         hidden_dim: int,
         output_dim: int,
         n_participants: int,
+        n_items: int,
         setting: str,
+        use_item_variance: bool,
         use_sampling: bool,
         n_samples: int,
         device=torch.device("cpu"),
@@ -197,7 +207,9 @@ class UnitRandomIntercepts(RandomInterceptsModel):
             hidden_dim,
             output_dim,
             n_participants,
+            n_items,
             setting,
+            use_item_variance,
             use_sampling,
             n_samples,
             device=device,
@@ -207,7 +219,10 @@ class UnitRandomIntercepts(RandomInterceptsModel):
         self.squashing_function = sigmoid
 
         # A fixed shift term for calculating nu when parametrizing the beta distribution
-        self.nu_shift = Parameter(torch.tensor([0.0]))
+        if self.use_item_variance:
+            self.nu_shift = Parameter(torch.zeros(self.n_items))
+        else:
+            self.nu_shift = Parameter(torch.tensor([0.0]))
 
         # There are two random effects per annotator (shift and variance)
         self.mean = torch.zeros(2)
@@ -227,7 +242,13 @@ class UnitRandomIntercepts(RandomInterceptsModel):
     def _random_effects(self):
         return self.random_effects - self.random_effects.mean(0)[None, :]
 
-    def _link_function(self, fixed, random, participant):
+    def _link_function(self, fixed, random, participant, item):
+
+        if item and self.use_item_variance:
+            nu_shift = self.nu_shift[item]
+        else:
+            nu_shift = self.nu_shift
+
         # Standard setting: (text, hypothesis) --> label
         if self.setting == "standard" or random is None:
 
@@ -237,7 +258,7 @@ class UnitRandomIntercepts(RandomInterceptsModel):
 
             # Mu and nu used to calculate the parameters for the beta distribution
             mu = mean
-            nu = torch.exp(self.nu_shift)
+            nu = torch.exp(nu_shift)
 
         # Extended setting: (text, hypothesis, annotator) --> label.
         # Here, however, we handle the case where we lack annotator information
@@ -258,7 +279,7 @@ class UnitRandomIntercepts(RandomInterceptsModel):
 
             # Mu and nu used to calculate the parameters for the beta distribution
             mu = mean
-            nu = torch.exp(self.nu_shift + random_variance)
+            nu = torch.exp(nu_shift + random_variance)
 
         # Extended setting: (text, hypothesis, annotator) --> label
         else:
@@ -271,7 +292,7 @@ class UnitRandomIntercepts(RandomInterceptsModel):
 
             # Mu and nu used to calculate the parameters for the beta distribution
             mu = mean
-            nu = torch.exp(self.nu_shift + random_variance[participant])
+            nu = torch.exp(nu_shift + random_variance[participant])
 
         # Parameter estimates for the beta distribution. These are
         # estimated from the mean and precision.

nli_mixed_models/modules/nli_random_slopes.py
@@ -18,7 +18,9 @@ class RandomSlopesModel(NaturalLanguageInference):
         hidden_dim: int,
         output_dim: int,
         n_participants: int,
+        n_items: int,
         setting: str,
+        use_item_variance: bool,
         use_sampling: bool,
         n_samples: int,
         device=torch.device("cpu"),
@@ -29,7 +31,9 @@ class RandomSlopesModel(NaturalLanguageInference):
             hidden_dim,
             output_dim,
             n_participants,
+            n_items,
             setting,
+            use_item_variance,
             use_sampling,
             n_samples,
             device,
@@ -144,7 +148,7 @@ class RandomSlopesModel(NaturalLanguageInference):
         return predictions
 
     def forward(
-        self, embeddings, participant=None
+        self, embeddings, participant=None, item=None
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         """Do a forward pass on the model. Returns a tuple (prediction, random_loss), where
            random_loss is a loss associated with the prior over the random components
@@ -158,7 +162,7 @@ class RandomSlopesModel(NaturalLanguageInference):
             predictions = []
             for i in range(self.n_samples):
                 prediction_mlp = self._get_stacked_predictions(embeddings, participant)
-                predictions.append(self._link_function(prediction_mlp, participant))
+                predictions.append(self._link_function(prediction_mlp, participant, item))
             # If unit case, predictions will be (alpha, beta) tuples
             if all(isinstance(x, tuple) for x in predictions):
                 alpha = torch.stack([x[0] for x in predictions]).mean(0)
@@ -169,7 +173,7 @@ class RandomSlopesModel(NaturalLanguageInference):
         else:
             # Get predictions from MLPs and pass through link function
             prediction_mlp = self._get_stacked_predictions(embeddings, participant)
-            prediction = self._link_function(prediction_mlp, participant)
+            prediction = self._link_function(prediction_mlp, participant, item)
 
         return prediction, random_loss
 
@@ -182,7 +186,9 @@ class CategoricalRandomSlopes(RandomSlopesModel):
         hidden_dim: int,
         output_dim: int,
         n_participants: int,
+        n_items: int,
         setting: str,
+        use_item_variance: bool,
         use_sampling: bool,
         n_samples: int,
         device=torch.device("cpu"),
@@ -193,7 +199,9 @@ class CategoricalRandomSlopes(RandomSlopesModel):
             hidden_dim,
             output_dim,
             n_participants,
+            n_items,
             setting,
+            use_item_variance,
             use_sampling,
             n_samples,
             device,
@@ -218,7 +226,7 @@ class CategoricalRandomSlopes(RandomSlopesModel):
         self.random_effects = self._extract_random_slopes_params()
         return self.random_effects - self.random_effects.mean(0)[None, :]
 
-    def _link_function(self, predictions, participant):
+    def _link_function(self, predictions, participant, item):
         """Computes the link function for a given model configuration."""
 
         # 'predictions' contains the outputs from the individual annotator MLPs,
@@ -247,7 +255,9 @@ class UnitRandomSlopes(RandomSlopesModel):
         hidden_dim: int,
         output_dim: int,
         n_participants: int,
+        n_items: int,
         setting: str,
+        use_item_variance: bool,
         use_sampling: bool,
         n_samples: int,
         device=torch.device("cpu"),
@@ -258,7 +268,9 @@ class UnitRandomSlopes(RandomSlopesModel):
             hidden_dim,
             output_dim,
             n_participants,
+            n_items,
             setting,
+            use_item_variance,
             use_sampling,
             n_samples,
             device,
@@ -275,7 +287,10 @@ class UnitRandomSlopes(RandomSlopesModel):
         self._initialize_random_effects()
 
         # A fixed shift term for calculating nu when parametrizing the beta distribution
-        self.nu_shift = Parameter(torch.tensor([0.0]))
+        if self.use_item_variance:
+            self.nu_shift = Parameter(torch.zeros(self.n_items))
+        else:
+            self.nu_shift = Parameter(torch.tensor([0.0]))
 
     def _initialize_random_effects(self):
         # The random effects in the unit random slopes model consist not
@@ -293,11 +308,16 @@ class UnitRandomSlopes(RandomSlopesModel):
         self.weights -= self.weights.mean(0)[None, :]
         return self.weights, self.variance - self.variance.mean()
 
-    def _link_function(self, predictions, participant):
+    def _link_function(self, predictions, participant, item):
         # Same link function as for unit random intercepts, except that we
         # feed the outputs of the annotator-specific regression heads to the
         # squashing function.
         mean = self.squashing_function(predictions).squeeze(1)
+
+        if item and self.use_item_variance:
+            nu_shift = self.nu_shift[item]
+        else:
+            nu_shift = self.nu_shift
 
         # Mu and nu used to calculate the parameters for the beta distribution
         # Extended setting subtask (b): use mean variance across participants.
@@ -308,11 +328,11 @@ class UnitRandomSlopes(RandomSlopesModel):
             else:
                 random_variance = 0
             mu = mean
-            nu = torch.exp(self.nu_shift + random_variance)
+            nu = torch.exp(nu_shift + random_variance)
         # Extended setting subtask (a).
         else:
             mu = mean
-            nu = torch.exp(self.nu_shift + self.variance[participant])
+            nu = torch.exp(nu_shift + self.variance[participant])
 
         # Parameter estimates for the beta distribution. These are
         # estimated from the mean and variance.

nli_mixed_models/predict/nli_predict.py
@@ -51,6 +51,8 @@ class NaturalLanguageInferencePredict:
                 else:
                     participant = None
 
+                item = torch.LongTensor(items.item.values).to(self.device)
+
                 # Embed items
                 embedding = self.nli.embed(items)
 
@@ -58,14 +60,14 @@ class NaturalLanguageInferencePredict:
                 if isinstance(self.nli, UnitRandomIntercepts) or isinstance(
                     self.nli, UnitRandomSlopes
                 ):
-                    prediction, _ = self.nli(embedding, participant)
+                    prediction, _ = self.nli(embedding, participant, item)
                     alpha, beta = prediction
                     pred_mean = alpha / (alpha + beta)
                     pred_precision = alpha + beta
                     all_pred_mean = torch.cat((all_pred_mean, pred_mean))
                     all_pred_precision = torch.cat((all_pred_precision, pred_precision))
                 else:
-                    pred_mean, _ = self.nli(embedding, participant)
+                    pred_mean, _ = self.nli(embedding, participant, item)
                     all_pred_mean = torch.cat((all_pred_mean, pred_mean))
 
             return all_pred_mean, all_pred_precision

nli_mixed_models/trainers/nli_trainer.py
@@ -29,10 +29,12 @@ class NaturalLanguageInferenceTrainer:
     def __init__(
         self,
         n_participants: int,
+        n_items: int,
         embedding_dim: int = 768,
         n_predictor_layers: int = 2,
         hidden_dim: int = 128,
         setting: str = "extended",
+        use_item_variance: bool = False,
         use_sampling: bool = False,
         n_samples: int = 100,
         device="cpu",
@@ -41,7 +43,9 @@ class NaturalLanguageInferenceTrainer:
         self.n_predictor_layers = n_predictor_layers
         self.hidden_dim = hidden_dim
         self.n_participants = n_participants
+        self.n_items = n_items
         self.setting = setting
+        self.use_item_variance = use_item_variance
         self.device = torch.device(device)
         self.nli = self.MODEL_CLASS(
             embedding_dim,
@@ -49,7 +53,9 @@ class NaturalLanguageInferenceTrainer:
             hidden_dim,
             self.OUTPUT_DIM,
             n_participants,
+            n_items,
             setting,
+            use_item_variance,
             use_sampling,
             n_samples,
             device,
@@ -113,6 +119,8 @@ class NaturalLanguageInferenceTrainer:
                 else:
                     participant = None
 
+                item = torch.LongTensor(items.item.values).to(self.device)
+
                 # Get targets, modal responses, and item embeddings
                 target = self.TARGET_TYPE(items.target.values).to(self.device)
                 modal_response = self.TARGET_TYPE(items.modal_response.values).to(
@@ -121,7 +129,7 @@ class NaturalLanguageInferenceTrainer:
                 embedding = self.nli.embed(items).to(self.device)
 
                 # Get model prediction and random loss (converting the latter as appropriate)
-                prediction, random_loss = self.nli(embedding, participant)
+                prediction, random_loss = self.nli(embedding, participant, item)
                 random_loss = (
                     random_loss
                     if isinstance(random_loss, float)